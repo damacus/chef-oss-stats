b752cd24c8185d07b3baa53588c721dfd82cad5e
Dan Webb <dan.webb@damacus.io>
2026-07-14 09:48:29 +0100

    feat: automate weekly reports
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- scripts/assemble_slack_report.rb
+++ scripts/assemble_slack_report.rb
@@ -2,8 +2,27 @@
 
 require 'erb'
 require 'date'
+require 'fileutils'
 require 'optparse'
 require 'mixlib/shellout'
+
+INTRO_TEXT = "Here's this week's Chef OSS community report.".freeze
+
+def markdown_links_to_mrkdwn(text)
+  text.gsub(/\[([^\]]+)\]\((https?:[^)]+)\)/, '<\\2|\\1>')
+end
+
+def present_text(text)
+  stripped = text.to_s.strip
+  stripped.empty? ? nil : "#{stripped}\n"
+end
+
+def normalize_spacing(text)
+  text
+    .gsub(/[ \t]+\n/, "\n")
+    .gsub(/\n{3,}/, "\n\n")
+    .strip + "\n"
+end
 
 options = {}
 OptionParser.new do |opts|
@@ -15,16 +34,26 @@ OptionParser.new do |opts|
   ) do |d|
     options[:date] = d
   end
+  opts.on(
+    '--output PATH',
+    'Write the assembled Slack report to PATH.',
+  ) do |path|
+    options[:output] = path
+  end
 end.parse!
 
 date = options[:date] || Date.today.to_s
-# rubocop:disable Lint/UselessAssignment
-promises = File.read("promise_reports/#{date}.md").lines.reject do |line|
-  line.strip.empty?
-end.join
-pipelines = File.read("pipeline_visibility_reports/#{date}.md").lines[2..].join
+intro = INTRO_TEXT
+promises = present_text(
+  File.read("promise_reports/#{date}.md").lines.reject do |line|
+    line.strip.empty?
+  end.join,
+)
+pipelines = present_text(
+  File.read("pipeline_visibility_reports/#{date}.md").lines[2..].join,
+)
 s = Mixlib::ShellOut.new('./bin/meeting_stats -m summary')
-meetings = s.run_command.stdout
+meetings = present_text(s.run_command.stdout)
 repos = ''
 File.read("repo_reports/#{date}.md").each_line do |line|
   next unless line.start_with?('*_[')
@@ -35,9 +64,19 @@ File.read("repo_reports/#{date}.md").each_line do |line|
     puts "this line should have matched: #{line}"
   end
 end
-external_contributions = File.read("external_contributors_reports/#{date}.md")
-# rubocop:enable Lint/UselessAssignment
+repos = present_text(repos)
+external_contributions = present_text(
+  File.read("external_contributors_reports/#{date}.md"),
+)
 
 template_file = File.expand_path('../templates/slack_report.erb', __dir__)
 template = File.read(template_file)
-puts ERB.new(template, trim_mode: '%<>').result
+output = ERB.new(template, trim_mode: '%<>').result(binding)
+output = normalize_spacing(markdown_links_to_mrkdwn(output))
+
+if options[:output]
+  FileUtils.mkdir_p(File.dirname(options[:output]))
+  File.write(options[:output], output)
+else
+  puts output
+end
